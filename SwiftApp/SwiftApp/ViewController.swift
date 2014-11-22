81fdc6a280f1d963dd3bd42796941fd6878bd119
Erick Chali <erickdemon@gmail.com>
2014-11-22 02:39:55 -0600

    Primera Aplicacion
    Obtener los años gato, :3

--- SwiftApp/SwiftApp/ViewController.swift
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //jalar el objeto, y se crea como variable.
+    @IBOutlet var txtEdadGato: UITextField!
+    
+    @IBOutlet var lblResultado: UILabel!
+    
+    @IBAction func btnObtenerEdad(sender: AnyObject) {
+        
+        var edad = txtEdadGato.text.toInt()
+        edad = edad!*7
+        lblResultado.text = "Su gato tiene \(edad!) anios gato."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //linea que utiliza la variable creada y cambia el label.
     }
 
     override func didReceiveMemoryWarning() {
